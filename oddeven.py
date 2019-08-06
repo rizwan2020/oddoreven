27f1ce038a430000d33c0bdf1c1e9e4f7cd40bcb
rizwan2020 <53808080+rizwan2020@users.noreply.github.com>
2019-08-06 12:24:42 +0530

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -1,5 +1,8 @@
 num = int(input(" "))
-if num%2==0
-  print("even")
-else
-  print("odd")
+if num > 0:
+    if num % 2 == 0:
+     print("even")
+    else:
+     print("odd")
+else:
+  print("invalid number")
